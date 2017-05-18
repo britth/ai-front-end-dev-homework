695fef30e12f532318f01e587674b06c84e1744e
britth <brittmhayes@gmail.com>
2017-05-18 11:54:09 -0400

    Fix issue to make animation appear at the same time comment is displayed

--- hw2-displaying-api-content/displaying-api-content.js
+++ hw2-displaying-api-content/displaying-api-content.js
@@ -4,7 +4,7 @@ $(function() {
 var position = 0
 var userId = 1
 
-  var getUserInfo = function() {
+  var getUserInfo = function(userId) {
     $.ajax({
       url: 'https://jsonplaceholder.typicode.com/users',
       method: 'GET',
@@ -14,7 +14,6 @@ var userId = 1
         $('.target-2').html('user unknown');
         $('.target-3').html('');
       } else {
-        console.log(data[userArrayPosition]["id"]);
         console.log(data[userArrayPosition]["name"]);
         console.log(data[userArrayPosition]["username"]);
         console.log(data[userArrayPosition]["email"]);
@@ -35,21 +34,23 @@ var userId = 1
       url: 'https://jsonplaceholder.typicode.com/posts?userId=' + userId,
       method: 'GET',
     }).then(function(data) {
-
-      if (position < data.length - 1) {
+      if (position < (data.length - 1)) {
         console.log(data[position]["body"] + ' (userId: ' + data[position]["userId"] + ')');
+        console.log(data[position]["id"])
         $('.target').html(data[position]["body"]);
-        getUserInfo();
+        getUserInfo(userId);
         $('.example-2').html('');
         position = position + 1;
-      }
-       else {
-        $('.example-2').html('Last post by this user');
+      } else if (position < data.length) {
         console.log('last post for this user: ', userId);
+        console.log(data[position]["body"] + ' (userId: ' + data[position]["userId"] + ')');
+        console.log(data[position]["id"])
+        $('.target').html(data[position]["body"]);
+        getUserInfo(userId);
+        $('.example-2').html('Last post by this user');
         position = 0;
         userId = userId + 1;
       }
-
     }).catch(function(err) {
       // try bad endpoint, see an error
       console.log('we got an error!');
